386dae3d17cb9ddcb33191a58a759cf222706574
Robson <robsonoliveira@gmail.com>
2020-07-31 11:10:29 -0300

    changing product to offer

--- models/Offer.model.js
+++ models/Offer.model.js
@@ -1,19 +1,19 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const productSchema = new Schema({
+const offerSchema = new Schema({
   title: String,
   value: Number,
-  state: Boolean,
+  active: Boolean,
   photo: String,
   availableQty: Number,
-  reservationLimitPeriod: String,
+  reservationLimitPeriod: Date,
   owner: {
     type: Schema.Types.ObjectId,
     ref: "User",
   },
 });
 
-const Product = mongoose.model("Product", productSchema);
+const Offer = mongoose.model("Offer", productSchema);
 
 module.exports = Product;
