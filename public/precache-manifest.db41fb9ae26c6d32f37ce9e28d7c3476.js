50564825af9c4e19e17fc12e31d6bdaa7c834a0f
Robson <robsonoliveira@gmail.com>
2020-08-06 15:43:51 -0300

    auth to heroku done

---
+++ public/precache-manifest.db41fb9ae26c6d32f37ce9e28d7c3476.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b30b52114ef25fc2fbef2424c832af43",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7903c929ea5d9dc89551",
+    "url": "/static/css/main.1b99e9f9.chunk.css"
+  },
+  {
+    "revision": "9951924a2bd9b42265a4",
+    "url": "/static/js/2.09c7d547.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.09c7d547.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7903c929ea5d9dc89551",
+    "url": "/static/js/main.10b30a67.chunk.js"
+  },
+  {
+    "revision": "5945b6c57e261f55b797",
+    "url": "/static/js/runtime-main.adf8137d.js"
+  }
+]);
